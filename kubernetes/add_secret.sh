d7f1d53393fc1025b7747129a2fc3a7c2f3e592c
rakeshpagi <rakesh_prog@yahoo.co.in>
2022-09-17 11:41:29 +0530

    REPLICAS AS 2

--- kubernetes/add_secret.sh
+++ kubernetes/add_secret.sh
@@ -3,4 +3,4 @@
 echo "Add Secret JFRONCRED"
 $SECRET_PASSWORD
 $DOCKER_EMAIL=
-kc create secret docker-registry jfrogcred --docker-registry=${DOCKER_EMAIL} --docker-username=${DOCKER_EMAIL} --docker-password=${SECRET_PASSWORD} --docker-email=${DOCKER_EMAIL}
+kc create secret docker-registry jfrogcred --docker-server=${DOCKER_EMAIL} --docker-username=${DOCKER_EMAIL} --docker-password=${SECRET_PASSWORD} --docker-email=${DOCKER_EMAIL}
