276438a71a29ed6996ce56e20f975677a0f1fffd
rakeshpagi <rakesh_prog@yahoo.co.in>
2022-09-19 21:26:43 +0530

    Carecontext discover Test GATEWAY URL Missing

--- apps/microservice/ndhm-client/src/app/configuration.ts
+++ apps/microservice/ndhm-client/src/app/configuration.ts
@@ -2,5 +2,6 @@
 export const SESSION_URL=`https://dev.abdm.gov.in/gateway/v0.5/sessions`
 
 export const DOMAINS={
-        HEALTHID:{URL:'https://healthidsbx.abdm.gov.in/api'}
+        HEALTHID:{URL:'https://healthidsbx.abdm.gov.in/api'},
+        GATEWAY:{URL:'https://dev.abdm.gov.in/gateway'},
 }
